245bf94f197134e26e2e95c72511fae83c45100a
hawk09\15072015 <drago44x@gmail.com>
2019-04-30 18:41:06 +0300

    routing + cosmetics

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainTrackerComponent } from './main-tracker/main-tracker.component';
+import { UserInfoComponent } from './user-info/user-info.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainTrackerComponent },
+  { path: 'reports', component: UserInfoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
